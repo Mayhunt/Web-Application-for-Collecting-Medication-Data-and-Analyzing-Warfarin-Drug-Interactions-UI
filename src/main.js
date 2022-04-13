e1042ac4620148a513b8b519cbeebdcc0511b0ea
MeepoohSUW <99584996+MeepoohSUW@users.noreply.github.com>
2022-04-14 01:15:53 +0700

    Add V modal

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue';
 import Buefy from 'buefy';
 import feather from 'vue-icon';
+import VModal from 'vue-js-modal';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import 'buefy/dist/buefy.css';
+import 'vue-js-modal/dist/styles.css';
 
 Vue.use(Buefy);
+Vue.use(VModal);
 Vue.use(feather, 'v-icon');
 Vue.config.productionTip = false;
 Vue.config.productionTip = false;
