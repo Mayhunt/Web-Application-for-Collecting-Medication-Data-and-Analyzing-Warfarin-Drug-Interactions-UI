504c71dd00e2bfaa4e67d511a3a6b6c366fb356c
MeepoohSUW <99584996+MeepoohSUW@users.noreply.github.com>
2022-04-20 02:44:34 +0700

    router log out icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
     <template #brand>
       <b-navbar-item href="#">
         <router-link to="/home">
-        <b-icon pack="fas" size="is-medium" icon="home" type="is-white"> </b-icon>
+          <b-icon pack="fas" size="is-medium" icon="home" type="is-white"> </b-icon>
         </router-link>
       </b-navbar-item>
       <b-navbar-item>
@@ -19,16 +19,16 @@
         </router-link>
       </b-navbar-item>
       <b-navbar-item>
-          <router-link to="/allergic-drug">
-            <b-icon pack="mdi" icon="exclamation" type="is-primary"> </b-icon>
-            <span style="padding-left: 16px"></span><span>Allergy Drug</span>
-          </router-link>
+        <router-link to="/allergic-drug">
+          <b-icon pack="mdi" icon="exclamation" type="is-primary"> </b-icon>
+          <span style="padding-left: 16px"></span><span>Allergy Drug</span>
+        </router-link>
       </b-navbar-item>
       <hr />
       <b-navbar-item>
         <router-link to="/me">
-        <b-icon pack="mdi" icon="account" type="is-primary"> </b-icon>
-        <span style="padding-left: 16px"></span><span>Profile</span>
+          <b-icon pack="mdi" icon="account" type="is-primary"> </b-icon>
+          <span style="padding-left: 16px"></span><span>Profile</span>
         </router-link>
       </b-navbar-item>
       <hr />
@@ -39,7 +39,9 @@
         <div class="buttons" style="justify-content: center">
           <b-button type="is-ghost">Privacy Statement</b-button>
           <b-button type="is-ghost">Terms of Use</b-button>
-          <b-icon pack="mdi" icon="logout" type="is-primary"> </b-icon>
+          <router-link to="/sign-in">
+          <b-icon pack="mdi" icon="logout" type="is-primary" size="is-medium" ></b-icon>
+          </router-link>
         </div>
       </b-navbar-item>
     </template>
