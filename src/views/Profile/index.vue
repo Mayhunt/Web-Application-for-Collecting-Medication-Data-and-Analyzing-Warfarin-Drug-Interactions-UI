3f5f28592d5b2bcacdbdc06aece83ea756558872
MeepoohSUW <99584996+MeepoohSUW@users.noreply.github.com>
2022-04-26 05:33:59 +0700

    edit button

--- src/views/Profile/index.vue
+++ src/views/Profile/index.vue
@@ -81,13 +81,13 @@
             class="fixedbuttons"
             style="justify-content: center; margin-top: 2.5rem; margin-bottom: 4rem"
           >
-            <div class="fixedbutton" style="justify-content: center">
+            <!-- <div class="fixedbutton" style="justify-content: center"> -->
               <router-link to="update-me">
                 <b-button rounded type="is-primary is-light" size="is-medium" expanded
                 >แก้ไข้ข้อมูล</b-button
                 ></router-link
               >
-            </div>
+            <!-- </div> -->
           </div>
         </form>
       </div>
