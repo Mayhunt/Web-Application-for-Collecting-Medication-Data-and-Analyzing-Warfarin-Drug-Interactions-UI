b9cd76a9fa9113f543c31c0de6111575eac57271
MeepoohSUW <99584996+MeepoohSUW@users.noreply.github.com>
2022-04-26 05:34:32 +0700

    change every hour to input number

--- src/views/EditDrugUsed/index.vue
+++ src/views/EditDrugUsed/index.vue
@@ -75,34 +75,10 @@
                     <b-checkbox size="is-medium" :value="false"> ก่อนนอน</b-checkbox>
                   </b-field>
                 </div>
-                <b-field label="ทุกๆ"
-                  ><b-select placeholder="จำนวนชั่วโมง" rounded>
-                    <option value="1">1 ชั่วโมง</option>
-                    <option value="2">2 ชั่วโมง</option>
-                    <option value="3">3 ชั่วโมง</option>
-                    <option value="4">4 ชั่วโมง</option>
-                    <option value="5">5 ชั่วโมง</option>
-                    <option value="6">6 ชั่วโมง</option>
-                    <option value="7">7 ชั่วโมง</option>
-                    <option value="8">8 ชั่วโมง</option>
-                    <option value="9">9 ชั่วโมง</option>
-                    <option value="10">10 ชั่วโมง</option>
-                    <option value="11">11 ชั่วโมง</option>
-                    <option value="12">12 ชั่วโมง</option>
-                    <option value="13">13 ชั่วโมง</option>
-                    <option value="14">14 ชั่วโมง</option>
-                    <option value="15">15 ชั่วโมง</option>
-                    <option value="16">16 ชั่วโมง</option>
-                    <option value="17">17 ชั่วโมง</option>
-                    <option value="18">18 ชั่วโมง</option>
-                    <option value="19">19 ชั่วโมง</option>
-                    <option value="20">20 ชั่วโมง</option>
-                    <option value="21">21 ชั่วโมง</option>
-                    <option value="22">22 ชั่วโมง</option>
-                    <option value="23">23 ชั่วโมง</option>
-                    <option value="24">24 ชั่วโมง</option>
-                  </b-select>
+                <b-field label="ทุกๆ">
+                  <b-input placeholder="6 ชั่วโมง (ใส่แค่ตัวเลข)" rounded trap-focus></b-input>
                 </b-field>
+                <br/>
               </div>
             </div>
           </section>
