444a950a5db1c46b9770e940237b5ab887d8d104
Mayhunt <kanwiya.may@gmail.com>
2022-05-26 09:35:22 +0700

    ....

--- src/views/ForTest/index.vue
+++ src/views/ForTest/index.vue
@@ -16,9 +16,15 @@
       >
         <div>
           <b-modal v-model="isImageModalActive">
-            <p class="image is-4by3">
-                <img src="/static/img/placeholder-1280x960.png">
-            </p>
+            <b-carousel>
+              <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
+                  <section :class="`hero is-medium is-${carousel.color}`">
+                      <div class="hero-body has-text-centered">
+                          <h1 class="title">{{carousel.text}}</h1>
+                      </div>
+                  </section>
+              </b-carousel-item>
+            </b-carousel>
         </b-modal>
           <div class="content is-medium">
             <!-- <br> -->
@@ -98,6 +104,13 @@ export default {
       username: '',
       password: '',
       isImageModalActive: true,
+      carousels: [
+        { text: 'Slide 1', color: 'primary' },
+        { text: 'Slide 2', color: 'info' },
+        { text: 'Slide 3', color: 'success' },
+        { text: 'Slide 4', color: 'warning' },
+        { text: 'Slide 5', color: 'danger' },
+      ],
     };
   },
   methods: {
